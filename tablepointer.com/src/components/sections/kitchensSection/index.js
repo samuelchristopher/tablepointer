a7e7c0aa6553d644a679e723d9c3a7f1b845169e
Samuel Paul Christopher <thesamchris@gmail.com>
2019-12-13 15:31:38 +0800

    responsive KitchensSection and filled icons

---
+++ tablepointer.com/src/components/sections/kitchensSection/index.js
@@ -0,0 +1,14 @@
+import { h } from 'preact'
+import style from './style'
+
+const KitchensSection = () => (
+    <div class={style.container}>
+        <div class={style.content}>
+            <h1>We Help Commercial Kitchens Take Control of Energy Costs</h1>
+            <p>TablePointer is the complete energy management partner for restaurants. The TablePointer system intelligently monitors and controls critical HVAC, refrigeration, lighting and ventilation equipment to actively save energy while providing complete visibility and control across your entire portfolio.</p>
+            <p>After analyzing your energy data, utility bills and operations, TablePointer experts will recommend the right combination of technology and behaviors to reduce your energy consumption by up to 30%.</p>
+        </div>
+    </div>
+)
+
+export default KitchensSection
--- tablepointer.com/src/routes/home/index.js
+++ tablepointer.com/src/routes/home/index.js
@@ -3,11 +3,13 @@ import style from './style'
 
 import CircleHeading from '../../components/circleHeading'
 import BenefitsSection from '../../components/sections/benefitsSection'
+import KitchensSection from '../../components/sections/kitchensSection'
 
 const Home = () => (
 	<div>
 		<CircleHeading headingText={"Your Complete Energy Partner"} showButton={true} buttonHref={"google.com"} buttonText={"Start Saving"}/>
 		<BenefitsSection />
+		<KitchensSection />
 	</div>
 );
 
