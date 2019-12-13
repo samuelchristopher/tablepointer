ac1e2def18b0c641f68a5d736887a02b0f2317e9
Samuel Paul Christopher <thesamchris@gmail.com>
2019-12-13 11:05:10 +0800

    updated header

--- tablepointer.com/src/components/header/index.js
+++ tablepointer.com/src/components/header/index.js
@@ -4,12 +4,9 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
+		<Link href="/">
+			<div class={style.logo}></div>
+		</Link>
 	</header>
 );
 
