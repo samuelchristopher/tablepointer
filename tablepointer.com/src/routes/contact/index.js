86859f00491911cdaa9de9c463293ac13bd658d9
Samuel Paul Christopher <thesamchris@gmail.com>
2019-12-16 16:07:12 +0800

    a different format for contact page

--- tablepointer.com/src/routes/contact/index.js
+++ tablepointer.com/src/routes/contact/index.js
@@ -1,26 +1,34 @@
 import { h } from 'preact'
 
 const Contact = () => (
-    <form name="contact" method="POST" data-netlify="true">
-        <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-            </select></label>
-        </p>
-        <p>
-            <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+  <div>
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
-)
+    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message"></textarea>
+    </form>
+  </div>
+);
 
 export default Contact
