b92cd4d052540527a569271a864d1e3337a05b1a
Samuel Paul Christopher <thesamchris@gmail.com>
2019-12-20 14:18:30 +0800

    testing ability to extract data from json file

--- tablepointer.com/src/routes/home/index.js
+++ tablepointer.com/src/routes/home/index.js
@@ -1,5 +1,6 @@
 import { h } from 'preact'
 import style from './style'
+import HomeContent from '../../../content/home.json'
 
 import CircleHeading from '../../components/circleHeading'
 import BenefitsSection from '../../components/sections/benefitsSection'
@@ -8,9 +9,12 @@ import BottomCTASection from '../../components/sections/bottomCTASection'
 import SolutionsSection from '../../components/sections/solutionSection'
 import DashboardSection from '../../components/sections/dashboardSection'
 
+console.log(HomeContent)
+
 const Home = () => (
 	<div>
 		<CircleHeading headingText={"Your Complete Energy Partner"} showButton={true} buttonHref="/contact" buttonText={"Start Saving"}/>
+		<p>this is from the home content: {HomeContent.title} </p>
 		<BenefitsSection />
 		<KitchensSection />
 		<DashboardSection />
