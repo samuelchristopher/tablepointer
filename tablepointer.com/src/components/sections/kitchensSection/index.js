75466606ab7069e6b2abe22e01b26d7059619d33
Samuel Paul Christopher <thesamchris@gmail.com>
2020-01-07 10:03:47 +0800

    integrated cms into kitchen section

--- tablepointer.com/src/components/sections/kitchensSection/index.js
+++ tablepointer.com/src/components/sections/kitchensSection/index.js
@@ -3,13 +3,13 @@ import style from './style'
 import HomeContent from '../../../../content/home.json'
 
 const KitchensSection = () => (
-    <div class={style.container}>
-        <div class={style.content}>
-            <h1>We Help Commercial Kitchens Take Control of Energy Costs</h1>
-            <p>TablePointer is the complete energy management partner for restaurants. The TablePointer system intelligently monitors and controls critical HVAC, refrigeration, lighting and ventilation equipment to actively save energy while providing complete visibility and control across your entire portfolio.</p>
-            <p>After analyzing your energy data, utility bills and operations, TablePointer experts will recommend the right combination of technology and behaviors to reduce your energy consumption by up to 30%.</p>
-        </div>
-    </div>
+	<div class={style.container}>
+		<div class={style.content}>
+			<h1>{HomeContent.kitchenSectionTitle}</h1>
+			<p>{HomeContent.kitchenSectionPOne}</p>
+			<p>{HomeContent.kitchenSectionPTwo}</p>
+		</div>
+	</div>
 )
 
 export default KitchensSection
