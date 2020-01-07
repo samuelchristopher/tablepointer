bae09140b4fb2ad4924e1627a65841336ae53cea
Samuel Paul Christopher <thesamchris@gmail.com>
2020-01-07 10:19:32 +0800

    integrated cms with solutionSection

--- tablepointer.com/src/components/sections/solutionSection/index.js
+++ tablepointer.com/src/components/sections/solutionSection/index.js
@@ -5,9 +5,9 @@ import HomeContent from  '../../../../content/home.json'
 
 const SolutionsSection = () => (
     <div class={style.container}>
-        <CircleHeading headingText="A Solution That Pays For Itself" showButton={false}/>
+        <CircleHeading headingText={HomeContent.solutionHeading} showButton={false}/>
         <div class={style.content}>
-            <p class={style.text}>No upfront costs and no risk. TablePointer simply shares in the money that it saves. This simple OpEx arrangement means that restaurants always save money while the additional operational benefits of the system help combat other critical expenses, such as rising wages and maintenance costs.</p>
+            <p class={style.text}>{HomeContent.solutionText}</p>
             <div class={style.image} />
         </div>
     </div>
